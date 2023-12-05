26e5b670dc8580f1161d7337c6942959f7b77b24
aarohinadkarni <arunadkarni@gmail.com>
2023-12-05 13:18:03 -0500

    first pass on nav & search

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,31 +1,60 @@
 import { Link, useLocation } from "react-router-dom";
 import { FaHome, FaSearch, FaUserCircle, FaSignInAlt } from "react-icons/fa";
+import { useAuth } from "../../hooks/useAuth";
+import porkify from './porkify.png';
+import "./index.css";
 
 function Navigation() {
-  const links = ["Home", "LogIn", "Profile", "Search"];
-  const icons = {
-    Home: <FaHome />,
-    LogIn: <FaSignInAlt />,
-    Profile: <FaUserCircle />,
-    Search: <FaSearch />,
-  };
+  const links_logged_in = ["Home", "Profile", "Search"];
+  const links_not_logged_in = ["Home", "Log In", "Create Account", "Profile", "Search"];
   const { pathname } = useLocation();
-  return (
-    <div className="list-group list-group-horizontal" style={{ width: 150 }}>
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          to={`/${link}`}
-          className={`list-group-item ${pathname.includes(link) && "active"}`}
-        >
-          <div class="flex align-items-center">
-            {icons[link]}
-            &nbsp;
-            {link}
+  const { user } = useAuth();
+
+  if (!user) {
+    return (
+      <div class="container porkify-navigation">
+        <div className="p-flex-row-container">
+          <div className="p-flex-row-container d-flex align-items-start col-4">
+            <img src={porkify}
+                style={{width:40}}
+            />
+            <h1>Porkify</h1>
           </div>
-        </Link>
-      ))}
-    </div>
-  );
+        <div className="list-group list-group-horizontal d-flex align-items-end col-8">
+          {links_not_logged_in.map((link, index) => (
+            <Link
+              key={index}
+              to={`/${link}`}
+              className={`list-group-item ${pathname.includes(link) && "active"}`}
+              style={{ whiteSpace: "nowrap" }}
+            >
+                {link}
+            </Link>
+          ))}
+        </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div class="container porkify-navigation">
+        <div className="p-flex-row-container">
+        <h1>Porkify</h1>
+        <div className="list-group list-group-horizontal">
+          {links_logged_in.map((link, index) => (
+            <Link
+              key={index}
+              to={`/${link}`}
+              className={`list-group-item ${pathname.includes(link) && "active"}`}
+              style={{ whiteSpace: "nowrap" }}
+            >
+              {link}
+            </Link>
+          ))}
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Navigation;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,9 @@
+function Search() {
+    return (
+        <div class="form-outline" data-mdb-input-init>
+  <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+</div>
+)
+}
+
+export default Search;
