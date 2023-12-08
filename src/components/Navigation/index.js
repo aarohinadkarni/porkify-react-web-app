838cd5c7d4dd39292ece4c67ff5ae0bbc0402fc4
Liang jimenez <liangjimenez@yahoo.com>
2023-12-07 20:30:00 -0500

    nav bar css

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
 import { FaHome, FaSearch, FaUserCircle, FaSignInAlt } from "react-icons/fa";
 import { useAuth } from "../../hooks/useAuth";
-import porkify from './porkify.png';
+import porkify from "./porkify-logo.png";
 import "./index.css";
 
 function Navigation() {
   const links_logged_in = ["Home", "Profile", "Search"];
-  const links_not_logged_in = ["Home", "Log In", "Create Account", "Profile", "Search"];
+  const links_not_logged_in = [
+    "HOME",
+    "LOG IN",
+    "CREATE ACCOUNT",
+    "PROFILE",
+    "SEARCH",
+  ];
   const { pathname } = useLocation();
   const { user } = useAuth();
 
@@ -14,26 +20,32 @@ function Navigation() {
     return (
       <div class="container porkify-navigation">
         <div className="row">
-          <div className="p-flex-row-container d-flex align-items-start col-md-4">
-            <img src={porkify}
-                style={{width:40}}
-                alt="porkify logo"
+          <div className="porkify-nav-heading p-flex-row-container d-flex align-items-start col-md-6">
+            <img
+              className="d-none d-md-block"
+              src={porkify}
+              style={{ width: 40 }}
+              alt="porkify logo"
             />
             &nbsp;
-            <h1>Porkify</h1>
-          </div>
-        <div className="list-group list-group-horizontal d-flex align-items-start col-md-8">
-          {links_not_logged_in.map((link, index) => (
-            <Link
-              key={index}
-              to={`/${link}`}
-              className={`list-group-item ${pathname.includes(link) && "active"}`}
-              style={{ whiteSpace: "nowrap" }}
-            >
-                {link}
+            <Link to={`/Home`} className="d-none d-md-block">
+              <h1>PORKIFY</h1>
             </Link>
-          ))}
-        </div>
+          </div>
+          <div className="p-nav-bar list-group list-group-horizontal d-flex align-items-start col-md-6 justify-content-end">
+            {links_not_logged_in.map((link, index) => (
+              <Link
+                key={index}
+                to={`/${link}`}
+                className={`list-group-item border-0 ${
+                  pathname.includes(link) && "active"
+                }`}
+                style={{ whiteSpace: "nowrap" }}
+              >
+                {link}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     );
@@ -41,26 +53,25 @@ function Navigation() {
     return (
       <div class="container porkify-navigation">
         <div className="row">
-          <div className="p-flex-row-container d-flex align-items-start col-md-4">
-            <img src={porkify}
-                style={{width:40}}
-                alt="porkify logo"
-            />
+          <div className="porkify-nav-heading p-flex-row-container d-flex align-items-start col-md-4">
+            <img src={porkify} style={{ width: 40 }} alt="porkify logo" />
             &nbsp;
             <h1>Porkify</h1>
           </div>
-        <div className="list-group list-group-horizontal d-flex align-items-end col-md-8">
-          {links_logged_in.map((link, index) => (
-            <Link
-              key={index}
-              to={`/${link}`}
-              className={`list-group-item ${pathname.includes(link) && "active"}`}
-              style={{ whiteSpace: "nowrap" }}
-            >
+          <div className="p-nav-bar list-group list-group-horizontal d-flex align-items-end col-md-8 justify-content-end">
+            {links_logged_in.map((link, index) => (
+              <Link
+                key={index}
+                to={`/${link}`}
+                className={`list-group-item ${
+                  pathname.includes(link) && "active"
+                }`}
+                style={{ whiteSpace: "nowrap" }}
+              >
                 {link}
-            </Link>
-          ))}
-        </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     );
