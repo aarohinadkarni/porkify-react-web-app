af7c809e694da84b1a633b86beb66956f27eb156
aarohinadkarni <arunadkarni@gmail.com>
2023-12-11 16:54:42 -0500

    change button type

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -65,7 +65,7 @@ export function Login() {
 
           <div>
             <button
-              type="submit"
+              type="button"
               className="flex w-full mt-12 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
               style={{backgroundColor:"#6B4A60"}}
               onClick={signin}
