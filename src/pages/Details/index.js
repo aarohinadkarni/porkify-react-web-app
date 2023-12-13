2828dac29cc03dad79e3653cdfa5f5498c31af11
nickel-dime <goepnik@gmail.com>
2023-12-12 23:47:59 -0500

    fixed both passing data and refreshing

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -5,15 +5,19 @@ import album from "./album-cover.jpeg";
 import "./index.css";
 import { FaRegHeart } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+
 // import * as userClient from "./users/client";
 // import * as reviewsClient from "./reviews/client";
 
 export function Details() {
   const { id } = useParams();
-  
+  const location = useLocation();
+  const { track } = location.state;
+
   //   const [currentUser, setCurrentUser] = useState(null);
   //   const [album, setAlbum] = useState(null);
-    const [song, setSong] = useState([]);
+  const [song, setSong] = useState([]);
   //   const { albumId } = useParams();
   //   const [likes, setLikes] = useState([]);
 
@@ -25,11 +29,6 @@ export function Details() {
   //       setCurrentUser(null);
   //     }
   //   };
-
-    const findSongById = async () => {
-      const song = await client.findSong(id);
-      setSong(song);
-    };
   //   const fetchTracks = async () => {
   //     const tracks = await client.findTracksByAlbumId(albumId);
   //     setTracks(tracks);
@@ -55,122 +54,128 @@ export function Details() {
   //     fetchLikes();
   //   }, []);
   useEffect(() => {
-      findSongById(id);
+    console.log(location.state);
   }, []);
+
   return (
-    <div>
-      <div className="song-details-header p-flex-row-container justify-content-center">
-        <div>
-          <img
-            src={album}
-            style={{ width: 300, height: 300 }}
-            alt="album cover"
-          />
-        </div>
-        <div>
-          <div className="p-flex-row-container">
-            {/* <h1 className="song-title">Let It Happen</h1> */}
-            <h1 className="song-title">{song.title}</h1>
-            {/* <h2 className="song-year">2023</h2> */}
-            <h2 className="song-year">{song.release_date}</h2>
-            {/* <h2 className="artist-name">By: Tame Impala</h2> */}
-            <h2 className="artist-name">By: {song.artists}</h2>
-          </div>
-          <div className="details-under-song-title">
-            <p>Details</p>
-            <hr></hr>
-            <ul>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Popularity
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-                <div className="bottom-labels">
-                  <label class="float-left">Playing at bars</label>
-                  <label class="float-right">World Tour</label>
-                </div>
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Energy
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-                <div className="bottom-labels">
-                  <label class="float-left">Playing at bars</label>
-                  <label class="float-right">World Tour</label>
-                </div>
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Vocals
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Tempo
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Danceable
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Mood
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-              </li>
-              <br></br>
-              <li>
-                <label for="customRange1" class="form-label">
-                  Acoustics
-                </label>
-                <input type="range" class="form-range" id="customRange1" />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <form className="review">
-          <div className="form-group like-button">
-            <button>
-              <FaRegHeart className="like-heart" />
-            </button>
-            <h3 className="like-title">Favorite</h3>
-          </div>
-          <div className="form-group rate-range">
-            <label for="customRange1" class="form-label">
-              Rate
-            </label>
-            <input type="range" class="form-range" id="customRange1" min="0" max="5" step="0.5" />
-            <div className="bottom-labels">
-                  <label class="float-left">0</label>
-                  <label class="float-right">5</label>
-                </div>
-          </div>
-          <div class="form-group">
-            <label className="leave-a-comment" for="leaveAComment">
-              Leave a comment
-            </label>
-            <textarea
-              class="form-control"
-              id="leaveAComment"
-              rows="4"
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <button className="btn submit-button">SUBMIT</button>
-          </div>
-        </form>
+    <div className="song-details-header p-flex-row-container justify-content-center">
+      <div>
+        <img
+          src={track.album.images[1].url}
+          style={{ width: 300, height: 300 }}
+          alt="album cover"
+        />
       </div>
+      <div>
+        <div className="p-flex-row-container">
+          {/* <h1 className="song-title">Let It Happen</h1> */}
+          <h1 className="song-title">{track.name}</h1>
+          {/* <h2 className="song-year">2023</h2> */}
+          <h2 className="song-year">{track.album.release_date}</h2>
+          {/* <h2 className="artist-name">By: Tame Impala</h2> */}
+          <h2 className="artist-name">By: {track.artists[0].name}</h2>
+        </div>
+        <div className="details-under-song-title">
+          <p>Details</p>
+          <hr></hr>
+          <ul>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Popularity
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+              <div className="bottom-labels">
+                <label className="float-left">Playing at bars</label>
+                <label className="float-right">World Tour</label>
+              </div>
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Energy
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+              <div className="bottom-labels">
+                <label className="float-left">Playing at bars</label>
+                <label className="float-right">World Tour</label>
+              </div>
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Vocals
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Tempo
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Danceable
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Mood
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+            </li>
+            <br></br>
+            <li>
+              <label htmlFor="customRange1" className="form-label">
+                Acoustics
+              </label>
+              <input type="range" className="form-range" id="customRange1" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <form className="review">
+        <div className="form-group like-button">
+          <button>
+            <FaRegHeart className="like-heart" />
+          </button>
+          <h3 className="like-title">Favorite</h3>
+        </div>
+        <div className="form-group rate-range">
+          <label htmlFor="customRange1" className="form-label">
+            Rate
+          </label>
+          <input
+            type="range"
+            className="form-range"
+            id="customRange1"
+            min="0"
+            max="5"
+            step="0.5"
+          />
+          <div className="bottom-labels">
+            <label className="float-left">0</label>
+            <label className="float-right">5</label>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="leave-a-comment" htmlFor="leaveAComment">
+            Leave a comment
+          </label>
+          <textarea
+            className="form-control"
+            id="leaveAComment"
+            rows="4"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <button className="btn submit-button">SUBMIT</button>
+        </div>
+      </form>
     </div>
 
     // <div>
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,81 +1,92 @@
-import { useState } from 'react';
-import './index.css';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import "./index.css";
+import { Link, useSearchParams } from "react-router-dom";
 
 export function Search() {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
 
-    const handleSearch = async () => {
+  const handleSearch = async (searchTerm) => {
+    const test = localStorage.getItem("token");
 
-        console.log("TOKEN", localStorage.getItem('token'))
-        const test = localStorage.getItem('token');        
-        console.log(typeof test);
-        const jsonString = JSON.parse(test);
-        console.log("PLEASE WORK");
-        console.log(jsonString.access_token);
+    const jsonString = JSON.parse(test);
 
-        try {
-            const response = await fetch(`https://api.spotify.com/v1/search?q=${searchTerm}&type=track`, {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jsonString.access_token}`,
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                setSearchResults(data.tracks.items); // Assuming the track items are in data.tracks.items
-            } else {
-                console.error('Failed to fetch data');
-            }
-        } catch (error) {
-            console.error('Error occurred while fetching data:', error);
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchTerm}&type=track`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jsonString.access_token}`,
+            "Content-Type": "application/json",
+          },
         }
-    };
+      );
 
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResults(data.tracks.items); // Assuming the track items are in data.tracks.items
+        setSearchParams({ term: searchTerm });
+      } else {
+        console.error("Failed to fetch data");
+      }
+    } catch (error) {
+      console.error("Error occurred while fetching data:", error);
+    }
+  };
 
-    return (
-        <div className="form-outline align-middle cursor">
-            <div className="row">
-                <div className="col-11">
-                    <input
-                        type="search"
-                        id="form1"
-                        className="form-control"
-                        placeholder="Search a song..."
-                        aria-label="Search"
-                        value={searchTerm}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="col-1">
-                    <button
-                        type="button" // Change to type="button" to prevent form submission
-                        className="btn text-white btn-md button-submit regular"
-                        onClick={handleSearch}
-                    >
-                        Submit
-                    </button>
-                </div>
-            </div>
-            <div>
-                {/* Display search results */}
-                {searchResults.map((track) => (
-                    <div key={track.id} style={{marginTop:20, marginBottom:20}}>
-                                        <Link
-                                            to={`/details/${track.id}`}
-                                            className="rounded-md no-underline bg-indigo-600 get-started-button px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                            >
-                                            {track.name}
-                                        </Link>
-                    </div>
-                ))}
-            </div>
+  useEffect(() => {
+    const term = searchParams.get("term");
+    console.log(term);
+    if (term) {
+      setSearchTerm(term);
+      handleSearch(term);
+    }
+  }, []);
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="form-outline align-middle cursor">
+      <div className="row">
+        <div className="col-11">
+          <input
+            type="search"
+            id="form1"
+            className="form-control"
+            placeholder="Search a song..."
+            aria-label="Search"
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
         </div>
-    );
+        <div className="col-1">
+          <button
+            type="button" // Change to type="button" to prevent form submission
+            className="btn text-white btn-md button-submit regular"
+            onClick={() => handleSearch(searchTerm)}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <div>
+        {/* Display search results */}
+        {searchResults.map((track) => (
+          <div key={track.id} style={{ marginTop: 20, marginBottom: 20 }}>
+            <Link
+              to={`/details/${track.id}`}
+              state={{ track: track }}
+              className="rounded-md no-underline bg-indigo-600 get-started-button px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {track.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
